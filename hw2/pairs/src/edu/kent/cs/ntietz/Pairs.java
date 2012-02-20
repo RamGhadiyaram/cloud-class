f846a3387e36328f35b4e2405bd6f6a686ad9a0c
Nicholas Tietz <ntietz@gmail.com>
2012-02-20 01:08:16 -0500

    Finished probability code.

--- hw2/pairs/src/edu/kent/cs/ntietz/Pairs.java
+++ hw2/pairs/src/edu/kent/cs/ntietz/Pairs.java
@@ -1,6 +1,8 @@
 package edu.kent.cs.ntietz;
 
 import java.io.IOException;
+import java.io.DataOutput;
+import java.io.DataInput;
 import java.util.*;
 
 import org.apache.hadoop.fs.Path;
@@ -11,10 +13,56 @@ import org.apache.hadoop.util.*;
 
 public class Pairs
 {
-
-    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable>
+    public static class KeyPair implements WritableComparable
     {
-        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
+        public String left;
+        public String right;
+
+        public void write(DataOutput out)
+        throws IOException
+        {
+            out.writeUTF(left);
+            out.writeUTF(right);
+        }
+
+        public void readFields(DataInput in)
+        throws IOException
+        {
+            left = in.readUTF();
+            right = in.readUTF();
+        }
+
+        public int compareTo(Object oOther)
+        {
+            if (oOther instanceof KeyPair)
+            {
+                KeyPair other = (KeyPair)oOther;
+                if (left.compareTo(other.left) != 0)
+                {
+                    return left.compareTo(other.left);
+                }
+                else if (right.equals("*"))
+                {
+                    if (other.right.equals("*"))
+                        return 0;
+                    else
+                        return -1;
+                }
+                else
+                {
+                    return right.compareTo(other.right);
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+
+    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, KeyPair, IntWritable>
+    {
+        public void map(LongWritable key, Text value, OutputCollector<KeyPair, IntWritable> output, Reporter reporter)
         throws IOException
         {
             String line = value.toString();
@@ -29,29 +77,54 @@ public class Pairs
 
             for (int i = 0; i < words.size(); ++i)
             {
+                int count = 0;
                 for (int j = 0; j < words.size(); ++j)
                 {
                     if (i != j)
                     {
-                        String pair = String.valueOf(words.get(i)) + " " + String.valueOf(words.get(j));
-                        output.collect(new Text(pair), new IntWritable(1));
+                        KeyPair pair = new KeyPair();
+                        pair.left = words.get(i);
+                        pair.right = words.get(j);
+                        output.collect(pair, new IntWritable(1));
+                        ++count;
                     }
                 }
+                KeyPair marginal = new KeyPair();
+                marginal.left = words.get(i);
+                marginal.right = "*";
+                output.collect(marginal, new IntWritable(count));
             }
         }
     }
 
-    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable>
+    public static class Reduce extends MapReduceBase implements Reducer<KeyPair, IntWritable, Text, Text>
     {
-        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter)
+        private int totalCounts = 0;
+
+        public void reduce(KeyPair key, Iterator<IntWritable> values, OutputCollector<Text, Text> output, Reporter reporter)
         throws IOException
         {
-            int sum = 0;
-            while (values.hasNext())
+            if (key.right.equals("*"))
             {
-                sum += values.next().get();
+                totalCounts = 0;
+                while (values.hasNext())
+                {
+                    totalCounts += values.next().get();
+                }
             }
-            output.collect(key, new IntWritable(sum));
+            else
+            {
+                int sum = 0;
+                while (values.hasNext())
+                {
+                    sum += values.next().get();
+                }
+                double probability = ((double)sum) / totalCounts;
+
+                String outputPair = key.left + "," + key.right;
+                String outputText = sum + "," + probability;
+                output.collect(new Text(outputPair), new Text(outputText));
+            }
         }
     }
 
@@ -61,7 +134,7 @@ public class Pairs
         JobConf conf = new JobConf(Pairs.class);
         conf.setJobName("wordcount");
 
-        conf.setOutputKeyClass(Text.class);
+        conf.setOutputKeyClass(KeyPair.class);
         conf.setOutputValueClass(IntWritable.class);
 
         conf.setMapperClass(Map.class);
