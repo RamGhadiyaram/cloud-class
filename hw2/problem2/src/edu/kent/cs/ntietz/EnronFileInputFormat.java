991d388cafc1856a9e7da6eda807bc8313750486
Nicholas Tietz <ntietz@gmail.com>
2012-02-23 02:28:12 -0500

    Began work on Enron email dataset.

--- hw2/problem2/src/edu/kent/cs/ntietz/EnronFileInputFormat.java
+++ hw2/problem2/src/edu/kent/cs/ntietz/EnronFileInputFormat.java
@@ -4,21 +4,28 @@ import java.io.*;
 
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.fs.*;
 
-/* 
- * This input format supports reading one sentence at a time.
+/*
+ * Like the normal file input format, but it doesn't allow files to be split.
+ * Each mapper will handle a whole file.
  */
-public class SentenceInputFormat
-    extends FileInputFormat<LongWritable, Text>
+public class EnronFileInputFormat extends FileInputFormat
 {
+    protected boolean isSplitable(FileSystem fs, Path filename)
+    {
+        return false;
+    }
+    
     public RecordReader<LongWritable, Text> getRecordReader( InputSplit split
                                                            , JobConf conf
                                                            , Reporter reporter
                                                            )
     throws IOException
     {
-        return new SentenceRecordReader(conf, (FileSplit)split);
+        return new EnronRecordReader(conf, (FileSplit)split);
     }
+
 
 }
 
