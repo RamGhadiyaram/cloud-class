157cdc649c64f890d828ab0e2973a4311f29dad3
Nicholas Tietz <ntietz@gmail.com>
2012-04-07 18:29:11 -0400

    Added job for generating data.

---
+++ hw3/src/edu/kent/cs/ntietz/kmeans/DataGeneratorJob.java
@@ -0,0 +1,35 @@
+package edu.kent.cs.ntietz.kmeans;
+
+import java.io.*;
+import java.util.*;
+
+public class DataGeneratorJob
+{
+    public static void main(String... args)
+    throws IOException
+    {
+        // TODO handle args nicely here. perhaps import apache commons for arg handling.
+
+        int numberOfCenters = Integer.valueOf(args[0]);
+        int numberOfPoints = Integer.valueOf(args[1]);
+        int numberOfComponents = Integer.valueOf(args[2]);
+        double lowerBound = Double.valueOf(args[3]);
+        double upperBound = Double.valueOf(args[4]);
+        long seed = 2012; // TODO change this, keep it fixed for testing
+
+        // TODO replace the rudimentary arg handling above
+
+        DataGenerator gen = new DataGenerator();
+
+        List<Point> dataset = gen.generate( numberOfCenters
+                                          , numberOfPoints
+                                          , numberOfComponents
+                                          , lowerBound
+                                          , upperBound
+                                          , seed
+                                          );
+
+        //
+    }
+}
+
