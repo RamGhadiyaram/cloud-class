041c137d631468836b9ba406070ab93f496c263d
Nicholas Tietz <ntietz@gmail.com>
2012-04-08 15:22:00 -0400

    Added interfaces to be implemented

--- hw3/src/edu/kent/cs/ntietz/kmeans/KMeansMapper.java
+++ hw3/src/edu/kent/cs/ntietz/kmeans/KMeansMapper.java
@@ -1,6 +1,11 @@
 package edu.kent.cs.ntietz.kmeans;
 
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+
 public class KMeansMapper
+extends MapReduceBase
+implements Mapper<LongWritable, Point, LongWritable, Point>
 {
 
 }
