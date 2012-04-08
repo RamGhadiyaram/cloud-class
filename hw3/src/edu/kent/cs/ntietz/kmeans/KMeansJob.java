44f6401160d5fe9aaeeedf310d53db5581904eb0
Nicholas Tietz <ntietz@gmail.com>
2012-04-08 15:15:37 -0400

    Added some imports, skeleton for job conf.

--- hw3/src/edu/kent/cs/ntietz/kmeans/KMeansJob.java
+++ hw3/src/edu/kent/cs/ntietz/kmeans/KMeansJob.java
@@ -1,14 +1,39 @@
 package edu.kent.cs.ntietz.kmeans;
 
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.mapred.*;
+
+import java.io.*;
+import java.util.*;
+
 public class KMeansJob
 {
     public static void main(String... args)
     {
-        /* args:
-         *  0   ->  
-         *  1   ->  
-         *  2   ->
-         */
+        // TODO handle args here
+        String inputPath = args[0];
+
+
+        JobConf conf = new JobConf(KMeansJob.class);
+        conf.setJobName("kmeans");
+
+        /*
+        conf.setMapOutputKeyClass(...);
+        conf.setMapOutputValueClass(...);
+        conf.setOutputKeyClass(...);
+        conf.setOutputValueClass(...);
+
+        conf.setMapperClass(...);
+        conf.setReducerClass(...);
+        */
+
+        conf.setInputFormat(SequenceFileInputFormat.class);
+        conf.setOutputFormat(SequenceFileOutputFormat.class);
+
+        conf.setNumReduceTasks(1);
+
+        FileInputFormat.setInputPaths(conf, new Path(inputPath));
+        //FileOutputFormat.setOutputPath(conf, new Path(outputPath));
     }
 }
 
